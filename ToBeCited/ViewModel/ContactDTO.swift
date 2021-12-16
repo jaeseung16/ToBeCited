3c4700c06890c58f48bf3cc18f1fa7bc2343e3f4
Jae-Seung Lee <jaeseung@gmail.com>
2021-12-16 15:52:28 -0500

    Feature: Add add(contact:to:viewContext:) to ToBeCitedViewModel
    Add ContactDTO

---
+++ ToBeCited/ViewModel/ContactDTO.swift
@@ -0,0 +1,14 @@
+//
+//  ContactDTO.swift
+//  ToBeCited
+//
+//  Created by Jae Seung Lee on 12/16/21.
+//
+
+import Foundation
+
+struct ContactDTO {
+    let email: String
+    let institution: String
+    let address: String
+}
