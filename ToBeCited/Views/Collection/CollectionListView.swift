a6b0cdd7de59aa6ef1e3982c50b8d2cb317dc757
Jae-Seung Lee <jaeseung@gmail.com>
2022-04-04 17:55:19 -0400

    Feature: Search Collection by name

--- ToBeCited/Views/Collection/CollectionListView.swift
+++ ToBeCited/Views/Collection/CollectionListView.swift
@@ -17,11 +17,24 @@ struct CollectionListView: View {
     private var collections: FetchedResults<Collection>
     
     @State private var presentAddCollectionView = false
+    @State private var titleToSearch = ""
+    
+    private var filteredCollections: [Collection] {
+        collections.filter { collection in
+            if titleToSearch == "" {
+                return true
+            } else if let name = collection.name {
+                return name.range(of: titleToSearch, options: .caseInsensitive) != nil
+            } else {
+                return false
+            }
+        }
+    }
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(collections) { collection in
+                ForEach(filteredCollections) { collection in
                     if let name = collection.name, name != "" {
                         NavigationLink(destination: CollectionDetailView(collection: collection, collectionName: name)) {
                             VStack {
@@ -43,6 +56,7 @@ struct CollectionListView: View {
                 .onDelete(perform: deleteCollections)
             }
             .navigationTitle("Collections")
+            .searchable(text: $titleToSearch)
             .toolbar {
                 ToolbarItem {
                     Button(action: {
@@ -62,7 +76,7 @@ struct CollectionListView: View {
     
     private func deleteCollections(offsets: IndexSet) {
         withAnimation {
-            viewModel.delete(offsets.map { collections[$0] }, viewContext: viewContext)
+            viewModel.delete(offsets.map { filteredCollections[$0] }, viewContext: viewContext)
         }
     }
 }
