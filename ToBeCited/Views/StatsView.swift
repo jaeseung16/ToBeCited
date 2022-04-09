5d8e7ed902fd123b531ca5d17e84a1d02316fc4f
Jae-Seung Lee <jaeseung@gmail.com>
2022-04-09 18:44:43 -0400

    Feature: Add StatsView
    TODO: Where to include StatsView?

---
+++ ToBeCited/Views/StatsView.swift
@@ -0,0 +1,25 @@
+//
+//  StatsView.swift
+//  ToBeCited
+//
+//  Created by Jae Seung Lee on 3/30/22.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    @EnvironmentObject private var viewModel: ToBeCitedViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Number of article: \(viewModel.articleCount)")
+            Text("Number of authors: \(viewModel.authorCount)")
+        }
+    }
+}
+
+struct StatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatsView()
+    }
+}
