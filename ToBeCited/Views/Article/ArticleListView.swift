81a3185b284d371466ec89ea66779c2556498b87
Jae-Seung Lee <jaeseung@gmail.com>
2024-09-29 13:36:08 -0400

    fix: Set selectedTab to stay in the same tab when presenting sheet
    (cherry picked from commit 2808a15ebe91b1280f4b9aeafa008ea72b3a43f5)

--- ToBeCited/Views/Article/ArticleListView.swift
+++ ToBeCited/Views/Article/ArticleListView.swift
@@ -116,5 +116,6 @@ struct ArticleListView: View {
         viewModel.articleSearchString = ""
         viewModel.selectedAuthors = nil
         viewModel.selectedPublishedIn = nil
+        viewModel.selectedTab = .articles
     }
 }
