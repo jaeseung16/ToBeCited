ebab0eba8bafd1769a08635335ec4bb3309693f7
Jae-Seung Lee <jaeseung@gmail.com>
2024-10-01 19:33:02 -0400

    feat: Dismiss pdf
    (cherry picked from commit dd65b0f08ce0cd172f06433f44ebcd7b8aa8c9af)

---
+++ ToBeCited/Views/Article/PDFReadView.swift
@@ -0,0 +1,33 @@
+//
+//  PDFView.swift
+//  ToBeCited
+//
+//  Created by Jae Seung Lee on 10/1/24.
+//
+
+import SwiftUI
+
+struct PDFReadView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State var pdfData: Data
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                PDFKitView(pdfData: pdfData)
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        dismiss.callAsFunction()
+                    } label: {
+                        Text("Dismiss")
+                    }
+                }
+            }
+        }
+
+    }
+}
